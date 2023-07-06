c20e9ffca941dcbb64662b709f02ecc4f2847b01
airada <23159600+airada@users.noreply.github.com>
2023-07-05 23:26:28 -0700

    Added first completed widget. Yay!

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ function createElement(tagname, classList) {
     return element;
 }
 
-function createWidgetElement(img_src) {
+function createWidgetElement(img_src, alt_text, url) {
     const container = createElement("a", ["col-12","col-md-6","col-lg-3","col-xl-4", "my-1", "my-md-2", "my-lg-3", "rounded-3"]);
-    const widget = createElement("div", ["card"]);
-    const widget_img = createElement("img", ["card-img-top"]);
+    const widget = createElement("div", ["card", "rounded-4"]);
+    const widget_img = createElement("img", ["card-img-top","rounded-4"]);
 
     //TO-DO: Update alt text and href to actual widget information.
     widget_img.src = img_src;
-    // widget_img.alt = "My cute dog named Inu."
-    // container.href = "https://jasmineirada.com/";
+    widget_img.alt = alt_text
+    container.href = url;
 
     widget.appendChild(widget_img);
     container.appendChild(widget);
@@ -24,7 +24,7 @@ function createWidgetElement(img_src) {
 
 const widget_group = document.getElementById("widgets");
 
-// let widget = createWidgetElement(img_src);
-// widget_group.appendChild(widget);
+let widget_daily_quote = createWidgetElement("assets/img/jirada-daily-quote.jpg", "Daily Quotation widget with iOS Banner notification interface.", "https://jasmineirada.com/jirada-daily-quote/");
+widget_group.appendChild(widget_daily_quote);
 
 
