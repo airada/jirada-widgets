c3a3bb40d8df5ce20ceb7c5d05d2dc2469c9a40d
airada <23159600+airada@users.noreply.github.com>
2023-07-06 11:12:47 -0700

    Updated Daily Quotations Widget card with new mockup image and card body title text.

--- script.js
+++ script.js
@@ -4,23 +4,29 @@ function createElement(tagname, classList) {
     return element;
 }
 
-function createWidgetElement(img_src, alt_text, url) {
+function createWidgetElement(title, img_src, alt_text, url) {
     const container = createElement("a", ["col-12","col-md-6","col-lg-3","col-xl-4", "my-1", "my-md-2", "my-lg-3", "rounded-3"]);
     const widget = createElement("div", ["card", "rounded-4"]);
     const widget_img = createElement("img", ["card-img-top","rounded-4"]);
-
+    const widget_body = createElement("div", ["card-body","border-top","rounded-bottom"]);
+    const widget_body_title = createElement("h5", ["card-title", "text-center"]);
+    
+    widget_body_title.textContent = title;
     widget_img.src = img_src;
     widget_img.alt = alt_text
     container.href = url;
 
+
+    widget_body.appendChild(widget_body_title);
     widget.appendChild(widget_img);
+    widget.appendChild(widget_body);
     container.appendChild(widget);
     return container;
 }
 
 const widget_group = document.getElementById("widgets");
 
-let widget_daily_quote = createWidgetElement("assets/img/jirada-daily-quote.jpg", "Daily Quotation widget with iOS Banner notification interface.", "https://jasmineirada.com/jirada-daily-quote/");
+let widget_daily_quote = createWidgetElement("Daily Quotations Widget","assets/img/jirada-daily-quote.jpg", "Daily Quotation widget with iOS Banner notification interface.", "https://jasmineirada.com/jirada-daily-quote/");
 widget_group.appendChild(widget_daily_quote);
 
 
